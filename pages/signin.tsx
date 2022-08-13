e0ac91b7eaaa234593c2f8dd16162200f57c8c9c
darshanmodi13 <202112070@daiict.ac.in>
2022-08-13 18:11:05 +0530

    commit

--- pages/signin.tsx
+++ pages/signin.tsx
@@ -7,7 +7,10 @@ const login = () => {
         <h1 className="tracking-[1rem] font-bold">TESLA</h1>
         <h1 className="tracking-wide font-bold ml-auto float-right">en-US</h1>
       </header>
-      <div className=" m-auto mt-24 w-[40%] pl-[10%]" id="login">
+      <div
+        className="absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]"
+        id="login"
+      >
         <h1 className="font-semibold text-4xl text-left">Sign In</h1>
         <p className="text-nav-light text-[14px] text-left mt-8 ml-4 font-semibold tracking-wider">
           Email Address
@@ -16,9 +19,11 @@ const login = () => {
           type="text"
           className="bg-offwhite outline-none p-2 pl-5 mt-3 rounded-[20px] w-80 focus:border-1 focus:outline-offwhite"
         />
+        <br />
+        <br />
         <button
           type="submit"
-          className="bg-blue outline-none p-2 pl-5 mt-4 rounded-[20px] w-80 text-white tracking-wide text-[13px]"
+          className="bg-blue outline-none p-2 mt-5 rounded-[20px] w-80 text-white tracking-wide text-[13px]"
         >
           NEXT
         </button>
