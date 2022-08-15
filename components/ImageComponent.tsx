dc8d6073d1ad7487b9d5a6c107318f1617106433
darshanmodi13 <202112070@daiict.ac.in>
2022-08-15 16:29:10 +0530

    image fixes

--- components/ImageComponent.tsx
+++ components/ImageComponent.tsx
@@ -6,10 +6,9 @@ const ImageComponent = () => {
       <div className="relative grid w-full h-[95vh] z-0">
         <Image
           src={img}
-          layout="responsive"
+          loading="lazy"
           alt="img"
           className="max-w-[100vw] max-h-[100vh] h-full w-full m-auto block object-cover"
-          priority={true}
         />
         <div className="absolute top-0 w-full h-full">
           <h1 className="z-20 text-center mt-24 text-[2.5rem] font-[500] tracking-wide">
@@ -19,7 +18,7 @@ const ImageComponent = () => {
             Order Online for{" "}
             <span className="border-b">Touchless Delivery</span>
           </p>
-          <div className="flex justify-center gap-5 text-[14px] absolute bottom-28 max-md:bottom-20 max-sm:bottom-16 w-full max-md:flex-col max-md:items-center">
+          <div className="flex justify-center gap-5 text-[14px] absolute bottom-28 max-md:bottom-20 w-full max-md:flex-col max-md:items-center">
             <button
               className="bg-btn-bg
              text-white pt-[6px] pb-[6px] border-1 rounded-[50px] pl-10 pr-10  w-52 tracking-wider"
